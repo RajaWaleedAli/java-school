bd44cdacdfe5aaca3ec0e80340c96e777888a187
RajaWaleedAli <129114133+RajaWaleedAli@users.noreply.github.com>
2024-11-13 13:10:09 +0100

    eule

--- November/src/Socken/Socke.java
+++ November/src/Socken/Socke.java
@@ -1,4 +1,16 @@
 package Socken;
 
 public class Socke {
+    String farbe;
+    boolean istTrocken;
+    Socke(String farbe) {
+        this.farbe = farbe;
+        this.istTrocken = true;
+    }
+    private boolean istTrocken() {
+        return istTrocken;
+    }
+    public void trockne(){
+        istTrocken = true;
+    }
 }
