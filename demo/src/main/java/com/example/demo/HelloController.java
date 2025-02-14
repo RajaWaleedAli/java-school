2df0493d5acf9d9bdd2473e7d9d4244ed060a30c
RajaWaleedAli <129114133+RajaWaleedAli@users.noreply.github.com>
2025-02-14 12:10:20 +0100

    währungsrechner

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,51 @@
 package com.example.demo;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private ChoiceBox<String> choiceBox;
+    @FXML
+    private ChoiceBox<String> choiceBox1;
+    @FXML
+    private TextField amount;
+    @FXML
+    private Button button;
+    @FXML
+    private Label label;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+
+        // Add items to the ChoiceBox
+        choiceBox.getItems().addAll("Euro", "US Dollar", "British Pound", "Japanese Yen", "Swiss Franc", "Canadian Dollar", "Australian Dollar", "Chinese Yuan", "Indian Rupee", "Brazilian Real");
+        choiceBox1.getItems().addAll("Euro", "US Dollar", "British Pound", "Japanese Yen", "Swiss Franc", "Canadian Dollar", "Australian Dollar", "Chinese Yuan", "Indian Rupee", "Brazilian Real");
+
+        // Set a default selected item
+        choiceBox.setValue("Euro");
+        choiceBox1.setValue("Euro");
+    }
+    @FXML
+    private void handleButtonClick() {
+        String selectedValue = choiceBox.getValue();
+        Waerungsrechner w=Waerungsrechner.createWaerungsrechner();
+        try{
+            double value=Double.parseDouble(amount.getText());
+            w.saveEur(selectedValue, value);
+            selectedValue = choiceBox1.getValue();
+            label.setText("" + w.umrechner(selectedValue));
+        }catch (NumberFormatException e){
+            System.err.println("NumberFormatException");
+        }catch (Exception e){
+            System.err.println(e.getMessage());
+        }
     }
 }
--- demo/src/main/java/com/example/demo/Waerungsrechner.java
+++ demo/src/main/java/com/example/demo/Waerungsrechner.java
@@ -4,13 +4,42 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 import java.awt.event.ActionEvent;
+import java.util.HashMap;
 
 public class Waerungsrechner {
-    @FXML
-    private Label waerung;
+    private HashMap<String,Double> hashMap=new HashMap<>();
+    private static boolean banane=false;
+    private static Waerungsrechner w;
+    private double waerung1;
 
-    @FXML
-    protected void chosenWaerung(ActionEvent event) {
-        waerung.setText(waerung.getText() + " ");
+    private Waerungsrechner(){
+        hashMap.put("Euro", 1.0);
+        hashMap.put("US Dollar", 1.048);
+        hashMap.put("British Pound", 0.83);
+        hashMap.put("Japanese Yen", 160.16);
+        hashMap.put("Swiss Franc", 0.95);
+        hashMap.put("Canadian Dollar", 1.42);
+        hashMap.put("Australian Dollar", 1.57);
+        hashMap.put("Chinese Yuan", 7.29);
+        hashMap.put("Indian Rupee", 87.50);
+        hashMap.put("Brazilian Real", 5.20);
     }
+
+    public static Waerungsrechner createWaerungsrechner(){
+        if(!banane){
+            Waerungsrechner.w=new Waerungsrechner();
+            Waerungsrechner.banane=true;
+            return w;
+        }else {
+            return w;
+        }
+    }
+    public void saveEur(String key,double value){
+        this.waerung1=value/hashMap.get(key);
+    }
+
+    public double umrechner(String s1){
+        return this.waerung1* hashMap.get(s1);
+    }
+
 }
